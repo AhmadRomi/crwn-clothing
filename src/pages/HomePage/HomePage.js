79a14236db1a0206a88dab5b00609719e112456b
hamada romi <ahmed.romi2020@gmail.com>
2020-05-02 02:15:12 +0300

    broken HomePage to smaller components and styling MenuItem

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directory/directory';
+import './HomePage.scss';
+
+const HomePage=()=>{
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    );
+}
+export  default HomePage;
